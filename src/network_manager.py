b27282bcd8697d0cf534086e8b921a5789b40117
Lorenzo Donini <lorenzo.donini@motius.de>
2017-09-04 13:38:31 +0200

    Add network manager for traffic monitoring and time sync

---
+++ src/network_manager.py
@@ -0,0 +1,30 @@
+from functools import reduce
+
+
+class Singleton(type):
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
+
+
+class NetworkManager(metaclass=Singleton):
+    def __init__(self):
+        self.monitored_traffic = {}  # <src, traffic>
+        self.current_time = 0
+
+    def total_generated_traffic(self):
+        return reduce(lambda total, val: total+val, self.monitored_traffic.values())
+
+    def generate_traffic(self, source, amount):
+        traffic = 0
+        if self.monitored_traffic[source]:
+            traffic = self.monitored_traffic[source] + amount
+        else:
+            traffic += amount
+        self.monitored_traffic[source] = traffic
+
+
+instance = NetworkManager()
