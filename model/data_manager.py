0a70bb9d981d6dc292cd9b03cd293bbc6330a356
Lorenzo Donini <lorenzo.donini@motius.de>
2017-09-05 16:30:17 +0200

    Update fieldnames for csv columns

--- model/data_manager.py
+++ model/data_manager.py
@@ -4,15 +4,15 @@ import csv
 class DataManager:
     def __init__(self, output_path):
         self.out_file = open(output_path, mode='w', newline='')
-        fieldnames = ['sim_time', 'nodes', 'services', 'generated_traffic', 'traffic_per_minute']
+        fieldnames = ['sim_time', 'nodes', 'services', 'generated_traffic_B', 'average_traffic_Bps']
         self.writer = csv.DictWriter(self.out_file, fieldnames=fieldnames)
         self.writer.writeheader()
 
-    def log_data(self, time, nodes, services_per_node, generated_traffic, traffic_per_minute):
+    def log_data(self, time, nodes, services_per_node, generated_traffic, traffic_per_second):
         row = dict()
         row['sim_time'] = time
         row['nodes'] = nodes
         row['services'] = services_per_node
-        row['generated_traffic'] = generated_traffic
-        row['traffic_per_minute'] = traffic_per_minute
+        row['generated_traffic_B'] = generated_traffic
+        row['average_traffic_Bps'] = traffic_per_second
         self.writer.writerow(row)
