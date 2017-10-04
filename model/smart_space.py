dceab5c572037e9ff1c129603b17a28fbf60636c
Lorenzo Donini <lorenzo.donini@motius.de>
2017-10-04 22:24:07 +0200

    Fix sum of traffic generated by requests

--- model/smart_space.py
+++ model/smart_space.py
@@ -19,7 +19,7 @@ class SmartSpace:
         if not node.is_service_running(service.service_id):
             return None
         new_certificate = self.slca.issue_certificate(node, service, network_manager.NetworkManager().current_time)
-        request_size = [random.randint(1, self.request_payload) * x for x in range(1, random.randint(1, self.access_groups))]
+        request_size = sum([random.randint(1, self.request_payload) * x for x in range(1, random.randint(1, self.access_groups))])
         network_manager.NetworkManager().generate_traffic(node.id, request_size)
         network_manager.NetworkManager().generate_traffic(node.id, new_certificate.size)  # SLSM -> NLSM
         return new_certificate
